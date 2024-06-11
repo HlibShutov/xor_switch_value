1c34511b9fc39c864295e4b348633e72c36b4f87
Hlib <u140_hlishu_waw@technischools.com>
2024-06-11 15:48:19 +0200

    change to binary

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const form = document.querySelector('form');
 const divs = document.querySelectorAll('div');
+
 valuesDiv = document.querySelector('.values');
 aValues = document.querySelector('.values > #a');
 bValues = document.querySelector('.values > #b');
+
+binaryValuesDiv = document.querySelector('.binary-values');
+aBinaryValues = document.querySelector('.binary-values > #a');
+bBinaryValues = document.querySelector('.binary-values > #b');
+
 
 divs.forEach((div) => div.hidden = true);
 
@@ -12,6 +18,15 @@ const switchValues = (event) => {
     b = Number(event.target.b.value);
     aValues.innerText = a;
     bValues.innerText = b;
+    a = a.toString(2);
+    b = b.toString(2);
+    if (a.length > b.length) {
+	b = b.padStart(a.length - b.length + 1, "0");
+    } else if (a.length < b.length) {
+        a = a.padStart(b.length - a.length + 1, "0");
+    }
+    aBinaryValues.innerText = a;
+    bBinaryValues.innerText = b;
 };
 
 form.addEventListener('submit', switchValues);
